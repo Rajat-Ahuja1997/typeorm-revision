89fa40f9b12f5c35788c6753c424f5b90469e43d
Rajat Ahuja <rahuja445@gmail.com>
2023-11-21 22:46:15 -0600

    fix declarations etc

--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { EntitySubscriberInterface } from 'typeorm';
-
-declare module 'typeorm-revision' {
-  export function createRevisionSubscriber<T>(entity: new () => T): EntitySubscriberInterface<T>;
-}
