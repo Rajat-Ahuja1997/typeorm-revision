eaa8888b138ccc9243a46da242bfbadb47756513
Rajat Ahuja <rahuja445@gmail.com>
2023-11-21 22:21:00 -0600

    remove dist

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createRevisionSubscriber = void 0;
-const createRevisionSubscriber_1 = require("./createRevisionSubscriber");
-Object.defineProperty(exports, "createRevisionSubscriber", { enumerable: true, get: function () { return createRevisionSubscriber_1.createRevisionSubscriber; } });
